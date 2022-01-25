a3ac52a6fa4f737722e7500b4ccc72a1e4930573
David G. Wonnacott <davew@cs.haverford.edu>
2022-01-25 18:09:38 -0500

    For next time, don't have the global function, yuck

--- typecheck.cpp
+++ typecheck.cpp
@@ -8,18 +8,8 @@
 //  to check types in tiger programs.
 //
 
-#define SYM_INFO_FOR_STDLIB
 
-void typecheck(AST_node_ *root)
-{
-	// This function should take care of doing type checking,
-	//  assuming "root" is the root of a tiger AST.
-	// It should call on some functions to define the attributes
-	//  related to type checking (and, in so doing, produce any necessary error messages).
-	
-}
-
-// The bodies of other type checking functions,
+// The bodies of type checking functions,
 //  including any virtual functions you introduce into
 //  the AST classes, should go here.
 
--- typecheck.h
+++ typecheck.h
@@ -1,6 +1,4 @@
 #ifndef TYPECHECK_H_
 #define TYPECHECK_H_
 
-void typecheck(AST_node_ *root);
-
 #endif /*TYPECHECK_H_*/
