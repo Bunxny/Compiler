cf0f18efaae046d92b54722a84c2c09d1ee2584f
David G. Wonnacott <davew@cs.haverford.edu>
2024-01-23 09:21:08 -0500

    Moved tests out to centralized script, just launch that

--- All_Tests.sh
+++ All_Tests.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
-WHERE=${HOME}/cs350/
-WHAT=${WHERE}/tiger/cmake-build-debug/tiger
-export TIGER_TEST_THERE=${WHERE}/A-folder-of-examples/Tiger-tests
-
-for d in davew others
-do
-    cd $TIGER_TEST_THERE/$d
-    if test "${#}" -eq 0
-    then
-      TESTS=*.tig
-    else
-      TESTS=${*}*.tig
-    fi
-
-    "${WHERE}"/A-folder-of-examples/Tiger-tests/Run-tests.sh ${TESTS}
-done
-
-
+TESTER=~/cs350/A-folder-of-examples/.bin/All_Tests.sh
+if test "${#}" -eq 0
+then
+  ${TESTER}
+else
+  ${TESTER} "$@"
+fi
